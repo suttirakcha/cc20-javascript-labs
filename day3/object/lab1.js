41517494cbc6ef8abcafbc0f8e76028c54cc3620
suttirakcha <suttirak.char@gmail.com>
2025-04-24 08:57:28 +0700

    Add object JS files

---
+++ day3/object/lab1.js
@@ -0,0 +1,14 @@
+const person = {
+  name: "Mark",
+  age: 20,
+  gender: "male",
+  address: {
+    street: "12345 Test Rd.",
+    district: "Ratchathewi",
+    province: "Bangkok",
+    postal_code: 10400
+  },
+  phone: "012-345-6789"
+}
+
+console.log(person);
---
+++ day3/object/lab4.js
@@ -0,0 +1,11 @@
+const username = "Mark";
+const email = "mark@mail.com";
+const password = "1234";
+
+const user = {
+  username,
+  email,
+  password
+}
+
+console.log(user);
